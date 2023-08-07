2c8e0a6e30ca579d50e27389dbb2236373758180
oggy107 <urmalsingh10@gmail.com>
2023-08-07 21:37:25 +0530

    added: suggestion

--- web3/contracts/Verify.sol
+++ web3/contracts/Verify.sol
@@ -11,6 +11,7 @@ contract Verify is Ownable {
 
     mapping (address => Manufacturer) public unverifiedManufacturers;
 
+    // TODO: suggestion: we can also use opeenzeppelins Access Control contracts to impliement this
     function addTrustedEntity(address trustedEntityAddress) external onlyOwner {
         require(!Array.exists(trustedEntities, trustedEntityAddress), "This entity is already trusted");
 
