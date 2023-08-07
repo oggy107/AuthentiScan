d284ef63999dd7c89b765c0914890a29d342c019
oggy107 <urmalsingh10@gmail.com>
2023-08-07 13:36:39 +0530

    added: AuthentiScan.sol

---
+++ web3/contracts/AuthentiScan.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+// interface AuthentiScanI {
+//     function registerManufacturer() external returns () {
+        
+//     }
+
+//     function verifyProduct(string id) external returns () {
+        
+//     }
+// }
+
+contract AuthentiScan {
+    struct Manufacturer {
+        string name;
+    }
+
+    constructor() {
+        
+    }
+
+    // function registerManufacturer() external returns () {
+        
+    // }
+
+    // function verifyProduct(string id)  returns () {
+        
+    // }
+}
