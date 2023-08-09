12fa1c0eb6de241a208773a5d8ee7ce92ade2ae6
oggy107 <urmalsingh10@gmail.com>
2023-08-09 16:44:43 +0530

    added: network configuration for ganache

--- web3/hardhat.config.ts
+++ web3/hardhat.config.ts
@@ -3,6 +3,13 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
     solidity: "0.8.21",
+
+    networks: {
+        ganache: {
+            url: "http://127.0.0.1:7545",
+            chainId: 1337,
+        },
+    },
 };
 
 export default config;
