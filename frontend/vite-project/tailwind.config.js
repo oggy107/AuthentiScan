579cabd926c3c2e580aa2d6f87db17d2aff85841
Jashanpreet Singh <jashanpreetsingh1096@gmail.com>
2023-08-13 16:14:05 +0530

    TS intialization

--- frontend/vite-project/tailwind.config.js
+++ frontend/vite-project/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",
-],
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
