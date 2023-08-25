d126a46836e60422e60fb6893bcee7d08ff56a8f
oggy107 <urmalsingh10@gmail.com>
2023-08-25 16:11:57 +0530

    added textarea input type and designed add product page

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -8,6 +8,9 @@ interface InputProps {
     className?: HTMLProps<HTMLElement>["className"];
     value?: HTMLProps<HTMLInputElement>["value"];
     onChange?: HTMLProps<HTMLInputElement>["onChange"];
+    textarea?: boolean;
+    rows?: HTMLProps<HTMLTextAreaElement>["rows"];
+    cols?: HTMLProps<HTMLTextAreaElement>["cols"];
 }
 
 const Input: FC<InputProps> = ({
@@ -18,18 +21,30 @@ const Input: FC<InputProps> = ({
     className,
     value,
     onChange,
+    textarea,
+    rows,
+    cols,
 }) => {
     return (
         <div className="w-full">
             <p className="text-neutral-500 text-base">{lable}</p>
-            <input
-                className={`w-full bg-purple-500 bg-opacity-5 rounded-lg shadow border border-black border-opacity-20 outline-none p-1 px-2 ${className}`}
-                type={type}
-                required={required}
-                name={name}
-                value={value}
-                onChange={onChange}
-            />
+            {textarea ? (
+                <textarea
+                    className={`w-full bg-purple-500 bg-opacity-5 rounded-lg shadow border border-black border-opacity-20 outline-none p-1 px-2 ${className}`}
+                    name={name}
+                    rows={rows}
+                    cols={cols}
+                ></textarea>
+            ) : (
+                <input
+                    className={`w-full bg-purple-500 bg-opacity-5 rounded-lg shadow border border-black border-opacity-20 outline-none p-1 px-2 ${className}`}
+                    type={type}
+                    required={required}
+                    name={name}
+                    value={value}
+                    onChange={onChange}
+                />
+            )}
         </div>
     );
 };
@@ -37,6 +52,8 @@ const Input: FC<InputProps> = ({
 Input.defaultProps = {
     required: false,
     type: "text",
+    textarea: false,
+    rows: 5,
 };
 
 export default Input;
--- frontend/src/routes/addProducts.tsx
+++ frontend/src/routes/addProducts.tsx
@@ -1,9 +1,155 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { useAccount } from "wagmi";
+
+import { Route } from "../types";
+import { useHeader } from "../context/HeaderContext";
+import useGetManufacturer from "../hooks/useGetManufacturer";
+import { RegistrationVMExceptions } from "../errors";
+import { toast } from "react-toastify";
+import fullLogo from "../assets/logoFull-dark.svg";
+import Input from "../components/Input";
+import Button from "../components/Button";
+
+const Info: FC = () => {
+    return (
+        <div className="w-full flex items-center justify-center">
+            <div className="w-[60%]">
+                <div>
+                    <img src={fullLogo} alt="authentiscan logo" />
+                </div>
+                <div className="text-neutral-500 mt-[41px]">
+                    AuthentiScan provides easy solutions for you to register
+                    your products on the blockchain. <br />
+                    <br />
+                    Simply fill up the product details and click on ‘Register On
+                    Blockchain’.You will be charged some gas fee for the
+                    transaction. Your products stays on the blockchain as long
+                    as blockchain stays, that is literally till eternity.
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const AddProductForm: FC = () => {
+    return (
+        <form className="p-5 mt-[20px] mx-[90px]">
+            <div className="text-gray-950 text-4xl font-bold mb-2">
+                Enter Product Details
+            </div>
+            {/* <div className="w-full flex gap-5"></div> */}
+            <div className="mt-[20px]">
+                <Input
+                    lable="Product Name"
+                    name="productName"
+                    // value={}
+                    // onChange={}
+                    required
+                />
+            </div>
+            <div className="mt-[20px]">
+                <Input
+                    lable="Product unique Id"
+                    name="productId"
+                    // value={}
+                    // onChange={}
+                    required
+                />
+            </div>
+            <div className="mt-[20px]">
+                <Input
+                    lable="Product Description"
+                    name="productDesc"
+                    // value={}
+                    // onChange={}
+                    required
+                    textarea
+                />
+            </div>
+            <div className="mt-[20px]">
+                <Input
+                    lable="MFG Date"
+                    name="productMFGDate"
+                    // value={}
+                    // onChange={}
+                    required
+                    type="date"
+                />
+            </div>
+            <div className="mt-[20px]">
+                <Input
+                    lable="Expiration Date"
+                    name="productEXPDate"
+                    // value={}
+                    // onChange={}
+                    type="date"
+                    required
+                />
+            </div>
+            <Button className="mt-4" title="Register Product" type="submit" />
+        </form>
+    );
+};
 
 const AddProduct: FC = (): JSX.Element => {
+    const { setNavLinks, setProfile } = useHeader();
+    const { address, isConnected } = useAccount();
+    const { manufacturer, isSuccess, isError, error } =
+        useGetManufacturer(address);
+
+    useEffect(() => {
+        setNavLinks([
+            {
+                name: "Home",
+                route: Route.HOME,
+            },
+            {
+                name: "Profile",
+                route: Route.PROFILE,
+            },
+            {
+                name: "Add Products",
+                route: Route.ADD_PRODUCTS,
+            },
+            {
+                name: "View Products",
+                route: Route.VIEW_PRODUCTS,
+            },
+        ]);
+    }, []);
+
+    useEffect(() => {
+        if (isError) {
+            if (
+                error?.message.includes(
+                    RegistrationVMExceptions.ManufacturerNotRegistered.Exception
+                )
+            ) {
+                toast.error(
+                    RegistrationVMExceptions.ManufacturerNotRegistered
+                        .ExceptionMessage
+                );
+            } else {
+                toast.error(error?.name);
+            }
+        }
+        if (isSuccess) {
+            if (manufacturer) {
+                setProfile({
+                    name: manufacturer.name,
+                    logo: manufacturer.logo,
+                });
+            }
+        }
+    }, [isError, isSuccess]);
     return (
         <div className="flex flex-grow">
-            <div className="w-full h-ful">AddProduct</div>
+            <div className="w-full h-ful">
+                <div className="w-full h-full grid grid-cols-2">
+                    <Info />
+                    <AddProductForm />
+                </div>
+            </div>
         </div>
     );
 };
