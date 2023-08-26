6acd636a8a7f910d39931d023b539f73602363af
Aslam Khan shah <aksi34012@gmail.com>
2023-08-26 14:56:12 +0530

    starting form

--- frontend/src/components/CheckAuthenicityForm.tsx
+++ frontend/src/components/CheckAuthenicityForm.tsx
@@ -1,7 +1,23 @@
 import { FC } from "react";
 
 const CheckAuthenicityForm: FC = () => {
-    return <form className="p-5 mt-[20px] mx-[90px]">yo</form>;
+    return <form className="p-5 mt-[20px] mx-[90px]">
+             
+
+             <div className="mt-[80px] ml-[70px] w-[386px] h-[429px]">
+              <div className="text-black text-4xl font-bold text-center w-[377px] h-[63px]">
+              Check Authenticity
+              </div>
+              <div className="text-gray-700 text-base font-normal w-[386px] h-[34px] mt-[-25px] text-center" >
+              Select manufacturer and product unique id to check the authenticity of your product.
+              </div>
+
+             </div>
+
+
+
+
+    </form>;
 };
 
 export default CheckAuthenicityForm;
