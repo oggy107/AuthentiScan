a0ad080e084e7255bc0793def458796ab55141cd
oggy107 <urmalsingh10@gmail.com>
2023-08-26 21:30:18 +0530

    reverted

--- frontend/src/components/CheckAuthenicityForm.tsx
+++ frontend/src/components/CheckAuthenicityForm.tsx
@@ -107,9 +107,12 @@ const CheckAuthenicityForm: FC = () => {
     };
 
     return (
-        <form className="p-5 mt-[20px] mx-[90px]">
-            <div className="mt-[80px] ml-[70px] w-[386px] h-[429px]">
-                <div className="text-black text-4xl font-bold text-center w-[377px] h-[63px]">
+        <form
+            onSubmit={hanldeSubmit}
+            className="p-5 mt-[20px] mx-[90px] w-full h-full flex items-center justify-center"
+        >
+            <div className="w-[50%]">
+                <div className="text-4xl font-bold text-center">
                     Check Authenticity
                 </div>
                 <div className="text-gray-700 mt-[25px] text-center">
@@ -126,24 +129,18 @@ const CheckAuthenicityForm: FC = () => {
                 </div>
 
                 <div className="mt-[26px]">
-                    <div className="text-black text-base font-normal">
-                        Enter Product ID
-                    </div>
-
-                    <div>
-                        <input
-                            className="pl-4 w-[383.45px] h-[43.74px] bg-purple-500 bg-opacity-5 rounded-lg border border-gray-300"
-                            type="number"
-                        />
-                    </div>
+                    <Input
+                        lable="Enter Product Id"
+                        name="productId"
+                        className="py-2"
+                        required
+                        value={productId}
+                        onChange={handleChange}
+                    />
                 </div>
 
-                <div className="mt-[58.26px] flex justify-end">
-                    <div className="w-[176px] h-[43px] bg-blue-900 rounded-[7px] flex items-center justify-center">
-                        <button className="text-white text-base font-normal text-center">
-                            CHECK
-                        </button>
-                    </div>
+                <div className="mt-[58.26px] w-full flex justify-end">
+                    <Button title="Check" type="submit" />
                 </div>
             </div>
         </form>
